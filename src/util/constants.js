dc1e4dc0df1982bbc207c23fd31c700d47b405ad
Rahmatxonov <rahmatxonov1001@gmail.com>
2023-12-17 09:08:55 +0500

    change

--- src/util/constants.js
+++ src/util/constants.js
@@ -2,32 +2,34 @@ import React from "react";
 
 import { AiFillHome, AiOutlineFlag } from "react-icons/ai";
 import { MdLocalFireDepartment, MdLiveTv } from "react-icons/md";
-import { CgMusicNote } from "react-icons/cg";
-import { FiFilm } from "react-icons/fi";
-import { IoGameControllerSharp } from "react-icons/io5";
-import { ImNewspaper } from "react-icons/im";
-import { GiDiamondTrophy, GiEclipse } from "react-icons/gi";
-import { RiLightbulbLine, RiFeedbackLine } from "react-icons/ri";
+import { MdSubscriptions } from "react-icons/md";
+import { FaRegFolder } from "react-icons/fa";
+import { FaRegFile } from "react-icons/fa";
+import { IoIosStopwatch } from "react-icons/io";
+import { FaRegHeart } from "react-icons/fa";
+import { IoMusicalNotesOutline } from "react-icons/io5";
 import { FiSettings, FiHelpCircle } from "react-icons/fi";
+import { MdFeedback } from "react-icons/md";
+import { CgMenuGridO } from "react-icons/cg";
 
 export const categories = [
-    { name: "New", icon: <AiFillHome />, type: "home" },
-    { name: "Trending", icon: <MdLocalFireDepartment />, type: "category" },
-    { name: "Music", icon: <CgMusicNote />, type: "category" },
-    { name: "Films", icon: <FiFilm />, type: "category" },
-    { name: "Live", icon: <MdLiveTv />, type: "category" },
-    { name: "Gaming", icon: <IoGameControllerSharp />, type: "category" },
-    { name: "News", icon: <ImNewspaper />, type: "category" },
-    { name: "Sports", icon: <GiDiamondTrophy />, type: "category" },
-    { name: "Learning", icon: <RiLightbulbLine />, type: "category" },
-    {
-        name: "Fashion & beauty",
-        icon: <GiEclipse />,
-        type: "category",
-        divider: true,
-    },
-    { name: "Settings", icon: <FiSettings />, type: "menu" },
-    { name: "Report History", icon: <AiOutlineFlag />, type: "menu" },
-    { name: "Help", icon: <FiHelpCircle />, type: "menu" },
-    { name: "Send feedback", icon: <RiFeedbackLine />, type: "menu" },
+  { name: "New", icon: <AiFillHome />, type: "home" },
+  { name: "Trending", icon: <MdLocalFireDepartment />, type: "category" },
+  { name: "Subscriptions", icon: <MdSubscriptions />, type: "category" },
+  { name: "Library", icon: <FaRegFolder />, type: "category" },
+  { name: "History", icon: <MdLiveTv />, type: "category" },
+  { name: "Watch later", icon: <FaRegFile />, type: "category" },
+  { name: "Favourites", icon: <IoIosStopwatch />, type: "category" },
+  { name: "Liked videos", icon: <FaRegHeart />, type: "category" },
+  { name: "Music", icon: <IoMusicalNotesOutline />, type: "category" },
+  {
+    name: "Fashion & beauty",
+    icon: <MdFeedback />,
+    type: "category",
+    divider: true,
+  },
+  { name: "Settings", icon: <FiSettings />, type: "menu" },
+  { name: "Report History", icon: <AiOutlineFlag />, type: "menu" },
+  { name: "Help", icon: <FiHelpCircle />, type: "menu" },
+  { name: "Send feedback", icon: <CgMenuGridO />, type: "menu" },
 ];
