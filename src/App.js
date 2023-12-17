8361c60de938d153395e51920393192866c2e84c
Rahmatxonov <rahmatxonov1001@gmail.com>
2023-12-17 18:54:26 +0500

    add text

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import React from 'react';
-import './App.css';
-import { AppContext } from './context/contextApi';
-import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import React from "react";
+import "./App.css";
+import { AppContext } from "./context/contextApi";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Feed from './component/Feed';
-import Header from './component/Header';
-import SearchResult from './component/SearchResult';
-import VideoDetails from './component/VideoDetails';
-
+import Feed from "./component/Feed";
+import Header from "./component/Header";
+import SearchResult from "./component/SearchResult";
+import VideoDetails from "./component/VideoDetails";
 
 function App() {
   return (
     <AppContext>
       <BrowserRouter>
-        <div className='flex flex-col h-full'>
+        <h1>Hello world</h1>
+        <div className="flex flex-col h-full">
           <Header />
           <Routes>
-            <Route path="/" exact element={<Feed />}/>
-            <Route path="/searchResult/:searchQuery" exact element={<SearchResult/>}/>
-            <Route path="/video/:id" exact element={<VideoDetails />}/>
+            <Route path="/" exact element={<Feed />} />
+            <Route
+              path="/searchResult/:searchQuery"
+              exact
+              element={<SearchResult />}
+            />
+            <Route path="/video/:id" exact element={<VideoDetails />} />
           </Routes>
         </div>
       </BrowserRouter>
